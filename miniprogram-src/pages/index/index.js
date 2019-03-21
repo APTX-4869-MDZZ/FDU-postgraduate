e2e269f60011ff72c58f4d31db0d404984894552
陈媛 <tangyuan@chenyuandeMacBook-Air.local>
2019-03-21 10:46:51 +0800

    静态页面

--- miniprogram-src/pages/index/index.js
+++ miniprogram-src/pages/index/index.js
@@ -4,17 +4,120 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '开启你的复旦之旅',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    wxBind: false,
+    first: false,
+    searchValue: '',
+    instruction: '选择你感兴趣的专业（可多选）：',
+    tags: [
+      {
+        isChosen: false,
+        text: '计算机科学'
+      },
+      {
+        isChosen: false,
+        text: '软件工程'
+      },
+      {
+        isChosen: false,
+        text: '微电子'
+      },
+      {
+        isChosen: false,
+        text: '通信工程'
+      },
+    ],
+    chosenTags: [],
+    current: "tuijian"
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+
+  bindTagTap: function(event) {
+    let text = event._relatedInfo.anchorTargetText
+    let newTags = []
+    for(let i=0; i<this.data.tags.length; i++){
+      let tag = this.data.tags[i]
+      if(tag.text === text){
+        tag.isChosen = !tag.isChosen
+        newTags.push(tag)
+      }
+      else{
+        newTags.push(tag)
+      }
+    }
+    this.setData({
+      tags: newTags
     })
   },
+
+  handleChange: function({detail}){
+    this.setData({
+      current: detail.key
+    })
+  },
+
+  bindInput: function(e){
+    this.setData({
+      searchValue: e.detail.value
+    })
+  },
+
+  bindSearchBtn: function(){
+    console.log(this.data.searchValue)
+    wx.navigateTo({
+      url: '../search/search',
+    })
+  },
+
+  bindConfirmTap: function(){
+    let tagArray = []
+    for (let i = 0; i < this.data.tags.length; i++) {
+      let tag = this.data.tags[i]
+      if(tag.isChosen){
+        tagArray.push(tag.text)
+      }
+    }
+    console.log(tagArray)
+    // this.setData({
+    //   instruction: '选择你感兴趣的方向（可多选）：',
+    //   tags:[
+    //     {
+    //       isChosen: false,
+    //       text: '微服务'
+    //     },
+    //     {
+    //       isChosen: false,
+    //       text: '大数据'
+    //     },
+    //     {
+    //       isChosen: false,
+    //       text: '可视化'
+    //     },
+    //     {
+    //       isChosen: false,
+    //       text: '计算机视觉'
+    //     },
+    //     {
+    //       isChosen: false,
+    //       text: '分布式'
+    //     },
+    //     {
+    //       isChosen: false,
+    //       text: '区块链'
+    //     }
+    //   ]
+    // })
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
+    this.setData({
+      first: false
+    })
+    wx.showTabBar({})
+  },
+
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
@@ -24,6 +127,7 @@ Page({
     } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
+      wx.hideTabBar({})
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -32,6 +136,7 @@ Page({
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.hideTabBar({})
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -48,7 +153,8 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      wxBind: true
     })
   }
 })
