0910c82bebd9e8153500979a186d5b15c6689431
APTX-4869-MDZZ <APTX-4869-MDZZ>
2019-04-04 13:33:47 +0800

    modify login

--- roadtoFDU/fdu/views.py
+++ roadtoFDU/fdu/views.py
@@ -13,14 +13,16 @@ def login(request):
     req_json = json.loads(request.read(), encoding='utf-8')
     js_code = req_json['code']
     url = 'https://api.weixin.qq.com/sns/jscode2session?appid=wxb42e944a8a905558&secret=0ab9ad927dd1caf7cc76779ab67891dc&js_code={}&grant_type=authorization_code'.format(js_code)
-    # wxresponse = requests.get(url)
-    # wxresponse_json = json.loads(wxresponse.read())
-    # print(wxresponse_json)
-    # if wxresponse['errcode'] == 0:
-    #     User.objects.create(openid=wxresponse_json['openid'], session_key=wxresponse_json['session_key'], unionid=wxresponse_json['unionid'])
-    # else:
-    #     print(wxresponse_json['errcode'], wxresponse_json['errmsg'])
+    wxresponse = requests.get(url)
+    wxresponse_json = json.loads(wxresponse.read())
+    print(wxresponse_json)
     res = {
         'success': True
     }
+    if 'errcode' in wxresponse_json:
+        User.objects.create(openid=wxresponse_json['openid'], session_key=wxresponse_json['session_key'], unionid=wxresponse_json['unionid'])
+        res['openid'] = wxresponse_json['openid']
+    else:
+        print(wxresponse_json['errcode'], wxresponse_json['errmsg'])
+        res['success'] = False
     return HttpResponse(res, content_type="application/json")
